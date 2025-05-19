d3a35b5c20b727a012f1be96d99836e109d474ce
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-05-19 09:58:11 -0300

    Msg assunto

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ const validoEmail = function() {return emailformat.test(document.getElementById(
 const validoNome = function() {return nameformat.test(document.getElementById("nome").value.toString())}
 
 function verificForm() {
-    
+    let formValido = true;
+    let assunto = document.getElementById("assunto").value.toString()
     if (!validoNome()) {
         document.getElementById("msg-nome").innerHTML = "Insira um nome válido, por exemplo: João Silva" 
+        formValido = false;
     }
     else {
         document.getElementById("msg-nome").innerHTML = ""
@@ -16,10 +18,17 @@ function verificForm() {
 
     if (!validoEmail()) {
         document.getElementById("msg-email").innerHTML = "Insira um endereço de e-mail válido, por exemplo: very.common@example.com."
-        return
+        formValido = false;
     }
     else {
         document.getElementById("msg-email").innerHTML = ""
+    }
+
+    if (assunto.length < 10 || assunto.length > 60) {
+        if (assunto.length < 10) document.getElementById("msg-assunto").innerHTML = "O assunto deve ter no mínimo 10 caracteres"
+        else document.getElementById("msg-assunto").innerHTML = "O assunto deve ter no máximo 60 caracteres"
+    } else {
+        document.getElementById("msg-assunto").innerHTML = ""
     }
 }
 
