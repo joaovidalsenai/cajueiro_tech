977516606101c8936b5195950250b5b617f704b8
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-05-19 09:39:01 -0300

    meu pc ta morrendo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const emailformat = /^(?!\.)(?!.*\.{2})[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@(?:[a-zA-Z0-9]+\.)+[a-zA-Z]{2,}$/
+const nameformat = /^[A-Za-zÀ-ÖØ-öø-ÿÇç]+(?:\s+[A-Za-zÀ-ÖØ-öø-ÿÇç]+)+$/
+
+const validoEmail = function() {return emailformat.test(document.getElementById("email").value.toString())}
+
+const validoNome = function() {return nameformat.test(document.getElementById("nome").value.toString())}
+
+function verificForm() {
+    if (!validoNome()) {
+        document.getElementById("msg-nome").innerHTML = "Insira um nome válido, por exemplo: João Silva"
+    }
+
+    if (!validoEmail()) {
+        document.getElementById("msg-email").innerHTML = "Insira um endereço de e-mail válido, por exemplo: very.common@example.com."
+    }
+}
+
